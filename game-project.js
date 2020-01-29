f14c4fab30e565e3f85b776ff3de8d1b6bb35e09
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-01-28 17:34:41 -0800

    Make some minor changes

--- game-project.js
+++ game-project.js
@@ -1,6 +1,3 @@
-function foo() {
-    let x = function () { return "foo!" };
-    return x();
-  }
-  /* anything here has no idea that x exists */
-  alert(foo());
+
+
+<button onclick="myFunction()">Click me</button>
